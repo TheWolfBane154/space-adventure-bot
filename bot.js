efa05e65f78a7cf5a54cfe94fb744f5707fc26ca
TheWolfBane154 <45442131+TheWolfBane154@users.noreply.github.com>
2019-02-08 14:17:45 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js")
+const bot = new Discord.Client()
+const fs = require("fs")
+
+fs.readdir("./commands/", (err, files) => {
+  console.log("Loading commands...");
+  if (err) return console.log(`Command loading failed!`);
+  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+  });
+});
+
+bot.on('message', message => {
+  let mArray = message.content.split(" ")
+  let args = mArray.slice(1)
+  let cmd = bot.commands.get(mArray[0].slice(prefix.length))
+  if (!message.content.startsWith(prefix)) return;
+  if (cmd) {
+    cmd.run(bot, message, args, Discord)
+console.log(`${message.author.username} used the ${message.content.split(" ")[0]} command.`)
+  }
+})
+
+bot.login(process.env.TOKEN)
